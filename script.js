8231ca74fcb24c6997f8be5419c8dab4d3a5479c
MarcosBR315 <marcospaulo.pontes05@gmail.com>
2022-07-01 17:39:46 -0300

    First English Challenge M2 - Movies with Objects

---
+++ script.js
@@ -0,0 +1,51 @@
+let comedy = [{
+    Title: "Grown Ups",
+    Director: "Dennis Dugan",
+    Year: 2010
+},
+{
+    Title: "Deadpool",
+    Director: "Tim Miller",
+    Year: 2016
+},
+{
+    Title: "Baywatch: SOS Malibu",
+    Director: "Seth Gordon",
+    Year: 2017
+}];
+
+
+let action = [{
+    Title: "Batman",
+    Director: "Matt Reeves",
+    Year: 2022
+},
+{
+    Title: "John Wick 3: Parabellum",
+    Director: "Chad Stahelski",
+    Year: 2019
+},
+{
+    Title: "John Wick",
+    Director: "Chad Stahelski",
+    Year: 2014
+}];
+let fantasy = [{
+    Title: "Alice in Wonderland",
+    Director: "Tim Burton",
+    Year: 2010
+},
+{
+    Title: "Tangled",
+    Director: ["Nathan Greno", "Byron Howard"],
+    Year: 2011
+},
+{
+    Title: "Shrek",
+    Director: ["Andrew Adamson", "Vicky Jenson"],
+    Year: 2001
+}];
+
+console.log("Action Movies: \n", action);
+console.log("Fantasy Movies: \n", fantasy);
+console.log("Comedy Movies: \n", comedy);
